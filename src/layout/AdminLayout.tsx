0516b9f03be96afadbf0f082b1295024c3d597c5
ohmprakashganesh <131688194+ohmprakashganesh@users.noreply.github.com>
2026-04-17 14:07:01 +0545

    layout

--- src/layout/AdminLayout.tsx
+++ src/layout/AdminLayout.tsx
@@ -5,7 +5,7 @@ import { Sidebar } from "../components/common/Sidebar";
 const AdminLayout: React.FC = () => {
   return (
 <div className="w-screen bg-gray-200 h-screen ">
-    <div className="h-screen flex max-w-7xl mx-auto bg-gray-200 overflow-hidden">
+    <div className="h-screen flex max-w-[2200px] mx-auto bg-gray-200 overflow-hidden">
       <Sidebar />
       <div className="flex-1 sticky flex flex-col overflow-hidden">
         <Navbar />
